5cb9c8a6ec3a751fa9fdf819c3d6d1bd0ad6ad2c
Patricia <Patricia@Patricia-PC>
2022-07-15 07:40:43 -0300

    ContaBancaria

--- AgenciaBancaria/src/Programa/Pessoa.java
+++ AgenciaBancaria/src/Programa/Pessoa.java
@@ -5,40 +5,38 @@ public class Pessoa {
 	private static int counter =1
 
 	private String nome;
-	private String CPF;
+	private String cpf;
 	private String email;
 	
 	
 	public Pessoa(String nome, String cPF, String email) {
-	
-		this.nome = nome;
-		CPF = cPF;
+	    this.nome = nome;
+		this. = cpf;
 		this.email = email;
-		
+		counter += 1; 
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getCPF() {
-		return CPF;
+		return cpf;
 	}
-
 	public void setCPF(String cPF) {
-		CPF = cPF;
+		this.cpf =cpf;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
+	public String toString(   ) {
+		return "\nNome: " + this.getNome() +
+				"\nCPF: " + this.getCPF() +
+				"\nEmail: " + this.getEmail()
+	}
 }
